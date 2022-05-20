ada93a62627ae5712172af922ffb271ec30e5af6
Sagetony <uchetony224@gmail.com>
2022-05-20 18:34:04 +0100

    Finished my seasonapp

--- seasonapp/src/SeasonDetail.js
+++ seasonapp/src/SeasonDetail.js
@@ -1,5 +1,16 @@
+import './SeasonDetail.css';
 import React from "react";
 
+const seasonConfig = {
+    summer:{
+        text: 'Omo hit the beach',
+        iconName: 'sun',
+    },
+    winter:{
+        text: 'It is Blurry',
+        iconName: 'snowflake',
+    }
+}
 const getSeason = (lat, month) =>{
     if(month > 2 && month < 9){
        return lat > 0 ? 'summer': 'winter';
@@ -8,12 +19,14 @@ const getSeason = (lat, month) =>{
     }
 }
 const SeasonDetail = (props) => {
-    const season = getSeason(props.latitude, new Date().getMonth() );
-    const text = season === 'winter' ? 'It is Blurry' : 'Omo hit the beach';
-
+    const season = getSeason(props.latitude, new Date().getMonth() );  
+    const {text, iconName} = seasonConfig[season];
         return (
-            <div>
+            <div className={`season-display ${season}`}>
+                <i className={`${iconName} icon massive icon-left`}></i>
                 <h1>{text}</h1>
+                <i className={`${iconName} icon massive icon-right`}></i>
+
             </div>
         );
 }
--- seasonapp/src/index.js
+++ seasonapp/src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDom from 'react-dom';
 import SeasonDetail from './SeasonDetail';
+import Spinner from './Spinner';
 
 class App extends React.Component{
   state = {lat: null, errorMessage: ''};
@@ -11,19 +12,22 @@ class App extends React.Component{
             (err) => this.setState({errorMessage: err.message}),   
          )
     }
-
-    render(){
-            if(this.state.lat && !this.state.errorMessage){
-                return <SeasonDetail latitude = {this.state.lat} />
-                // return <div>Latitude: {this.state.lat}</div>
+        renderContent(){
+            
+                if(this.state.lat && !this.state.errorMessage){
+                    return <SeasonDetail latitude = {this.state.lat} />
+                    // return <div>Latitude: {this.state.lat}</div>
+                }
+                if(this.state.errorMessage && !this.state.lat){
+                    return <div>Error: {this.state.errorMessage}</div>
+                }
+                return <Spinner message = "Please accept location" />
+        }
+            render(){
+                return <div className='border red'>{this.renderContent()}</div>;
+                
             }
-            if(this.state.errorMessage && !this.state.lat){
-                return <div>Error: {this.state.errorMessage}</div>
-            }
-            return <div>Loading</div>
-
-
-    }
+   
 }
 
 ReactDom.render(<App />, document.querySelector('#root'));

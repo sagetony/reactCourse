966c35a23b66efa16858da292d9973a099108eb3
Sagetony <uchetony224@gmail.com>
2022-05-23 12:22:27 +0100

    Updating my searchComponent

--- formapp/src/components/App.js
+++ formapp/src/components/App.js
@@ -1,12 +1,19 @@
 import React from "react";
 import SearchForm from './SearchForm';
 
-const App = () => {
-    return (
-            <div className="container ui" style={{ marginTop:'20px' }}>
-                <SearchForm />
-            </div>
-    );
+class App extends React.Component {
+
+    onSearchSubmit = (term) => {
+        console.log(term)
+    };
+    render(){
+            return (
+                <div className="container ui" style={{ marginTop:'20px' }}>
+                    <SearchForm onSubmit={this.onSearchSubmit} />
+                </div>
+        );
+    }
+  
 }
 
 export default App;
--- formapp/src/components/SearchForm.js
+++ formapp/src/components/SearchForm.js
@@ -2,10 +2,17 @@ import React from "react";
 
 class SearchForm extends React.Component{
     state = {textValue: ''};
+    
+     onSubmitForm = (event) => {
+            event.preventDefault();
+
+            this.props.onSubmit(this.state.textValue);
+    }
+
     render(){
         return (
             <div className="ui segment">
-                <form className="ui form">
+                <form className="ui form" onSubmit={this.onSubmitForm}>
                     <div className="field">
                         <label>Search</label>
                         <input type="text" name="first-name" value={this.state.textValue} onChange={ e => this.setState({textValue: e.target.value})}/>
